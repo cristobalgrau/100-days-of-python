dc347ec3fbc429a3c4effec4ec45477846f6f58e
Cristobal Grau <119089907+cristobalgrau@users.noreply.github.com>
2024-05-05 21:59:04 -0500

    Create scoreboard.py for turtle crossing game

---
+++ Intermediate/Day-23/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+FONT = ("Courier", 20, "bold")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.teleport(-250, 250)
+        self.level = 1
+        self.update_level()
+
+    def update_level(self):
+        self.clear()
+        self.level += 1
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.home()
+        self.write("GAME OVER", align="center", font=FONT)
