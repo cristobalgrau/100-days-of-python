e49c5d2904a932b818fb6dbdaaee9d1b0c7c7d81
Cristobal Grau <119089907+cristobalgrau@users.noreply.github.com>
2024-05-05 22:12:25 -0500

    Create main.py for snake game high score

---
+++ Intermediate/Day-24/snake_game/main.py
@@ -0,0 +1,49 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import ScoreBoard
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+score = ScoreBoard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        score.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        score.reset_game()
+        snake.reset_snake()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:  # slicing segments from 1 to the end, avoiding the head
+        if snake.head.distance(segment) < 10:
+            score.reset_game()
+            snake.reset_snake()
+
+
+screen.exitonclick()
