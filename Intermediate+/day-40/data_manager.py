b8954475741f792999dc1976b22ea246f40a94a0
Cristobal Grau <119089907+cristobalgrau@users.noreply.github.com>
2024-06-20 10:06:10 -0500

    Create data_manager.py for day 40

---
+++ Intermediate+/day-40/data_manager.py
@@ -0,0 +1,39 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class DataManager:
+    # This class is responsible for talking to the Google Sheet.
+    endpoint = os.environ.get("SHEETY_ENDPOINT")
+    users_endpoint = os.environ.get("SHEETY_USERS_ENDPOINT")
+    token = os.environ.get("SHEETY_TOKEN")
+
+    def __init__(self) -> None:
+        self.headers = {"Authorization": "Bearer " + self.token}
+
+    def get_cities(self):
+        response = requests.get(url=self.endpoint, headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()["prices"]
+
+    def update_iata_code(self, sheety_id, code):
+        put_endpoint = f"{self.endpoint}/{sheety_id}"
+
+        data = {
+            "price": {
+                "iataCode": code,
+            }
+        }
+
+        response = requests.put(url=put_endpoint, json=data, headers=self.headers)
+        response.raise_for_status()
+
+    def get_customer_emails(self):
+        response = requests.get(url=self.users_endpoint, headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()["users"]
